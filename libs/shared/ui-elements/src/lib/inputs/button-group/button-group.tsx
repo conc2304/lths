afd2f4ef1976febb7faae6cdafc58b68488bb0aa
Jose Conchello <jose.conchello@gmail.com>
2023-03-17 10:51:17 -0700

    clean up

--- libs/shared/ui-elements/src/lib/inputs/button-group/button-group.tsx
+++ libs/shared/ui-elements/src/lib/inputs/button-group/button-group.tsx
@@ -7,39 +7,13 @@ type Props = {
   buttons: Array<{ label: string; onClick: () => void }>;
 } & ButtonGroupProps;
 
-const StyledButtonGroup = styled(ButtonGroup)(({ theme }) => ({
-  '& .MuiButtonBase-root': {
-    border: '1px solid #D9D9D9',
-    textTransform: 'uppercase',
-    fontFamily: `'Roboto', sans-serif`,
-    fontWeight: 400,
-    fontSize: '0.9rem',
-    color: '#000',
-    padding: '9px 12px',
-    height: '34px',
-    backgroundColor: 'transparent',
-    transition: theme.transitions.create('background-color', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    '&:hover': {
-      backgroundColor: 'rgba(216, 216, 216, 0.4)',
-    },
-    '&.active': {
-      background: '#e3f3ff',
-    },
-  },
-  '& .MuiTouchRipple-child': {
-    backgroundColor: '#e3f3ff',
-  },
-}));
 
 export const LthsButtonGroup = ({
   color = 'primary',
   variant = 'outlined',
   ...rest
 }: Props) => {
-  const { buttons } = rest;
+  const { buttons, sx } = rest;
 
   const [activeIndex, setIsActiveIndex] = useState<number | null>(null);
 
@@ -54,6 +28,7 @@ export const LthsButtonGroup = ({
       color={color}
       disableElevation
       aria-label="outline button group"
+      sx={sx}
     >
       {buttons.map((buttonItem, i) => {
         return (
