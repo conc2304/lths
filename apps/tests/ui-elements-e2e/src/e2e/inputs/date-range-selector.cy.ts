1e75392e37b7c443c12328e5ec9ee851be1862d5
Jose Conchello <jose.conchello@gmail.com>
2023-03-24 15:37:31 -0700

    test functionality

--- apps/tests/ui-elements-e2e/src/e2e/inputs/date-range-selector.cy.ts
+++ apps/tests/ui-elements-e2e/src/e2e/inputs/date-range-selector.cy.ts
@@ -28,26 +28,37 @@ describe('shared-ui-elements: Date Range Input', () => {
     cy.get('.Lths-Date-Picker').should('have.length', 2);
   });
 
-  it("should set selected class after selecting a date range from the button group", () => {
-    cy.get(".MuiButtonBase-root").first().as("button");
 
-    cy.get("@button").should("exist");
-    cy.get("@button").should("not.have.class", "Mui-selected");
-    cy.get("@button").click();
-    cy.get("@button").should("have.class", "Mui-selected");
-    cy.getReact("DateRangeSelector")
-      .getCurrentState()
-      .should("have.property", "dateOptionGroupValue", "2");
+  it('should set selected toggle on click, and unset selected toggle button after selecting custom date', () => {
+    cy.get('.MuiButtonBase-root').first().as('button');
+    cy.get('input.MuiInputBase-input').first().as('input');
+    cy.get('.MuiInputAdornment-root button.MuiButtonBase-root').first().as('calenderButton');
 
-    // cy.get('@button')
-    //   .invoke('val')
-    //   .then((buttonValue) => {
-    //     // do more work here
-    //     console.log('HERE')
-    //     console.log('buttonValue', buttonValue)
-    //     console.log('HERE')
-    //     cy.getReact('DateRangeSelector').getCurrentState().should('have.property', 'dateOptionGroupValue', buttonValue)
+    cy.get('@input').should('have.value', '');
+    cy.get('@button').should('not.have.class', 'Mui-selected');
+    cy.get('@button').click();
+    cy.get('@button').should('have.class', 'Mui-selected');
 
-    //   });
+    cy.get('@calenderButton').click();
+    cy.get('button[data-timestamp].MuiPickersDay-root').first().click()
+
+    cy.get('@button').should('not.have.class', 'Mui-selected');
+  });
+
+  it('should clear the date input fields when a date option is selected', () => {
+    cy.get('.MuiButtonBase-root').first().as('button');
+    cy.get('input.MuiInputBase-input').first().as('input1');
+    cy.get('input.MuiInputBase-input').last().as('input2');
+
+    cy.get('@input1').invoke('val', '03 / 15 / 2023').trigger('change');
+    cy.get('@input2').invoke('val', '03 / 16 / 2023').trigger('change');
+
+    cy.get('@input1').should('have.value', '03 / 15 / 2023');
+    cy.get('@input2').should('have.value', '03 / 16 / 2023');
+
+    cy.get('@button').click();
+    cy.get('@button').should('have.class', 'Mui-selected');
+    cy.get('@input1').should('have.value', '');
+    cy.get('@input2').should('have.value', '');
   });
 });
