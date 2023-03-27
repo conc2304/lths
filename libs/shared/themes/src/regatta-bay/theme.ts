44b70b1662e9368d37bf1bbedf47b1cf22180921
Jose Conchello <jose.conchello@gmail.com>
2023-03-27 15:52:52 -0700

    move create theme to new file

---
+++ libs/shared/themes/src/regatta-bay/theme.ts
@@ -0,0 +1,40 @@
+import { createTheme, Theme, ThemeOptions } from '@mui/material';
+import { deDE as coreDeDE } from '@mui/material/locale'; // core translations
+import { deDE } from '@mui/x-date-pickers'; // translations for date picker
+import type {} from '@mui/x-date-pickers/themeAugmentation';
+import getPalette from './palette';
+import getTypography from './typography';
+import getComponentOverrides from './overrides/index';
+
+const palette = getPalette();
+const typography = getTypography(`'Public Sans', sans-serif`);
+
+const baseThemeOptions: ThemeOptions = {
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 768,
+      md: 1024,
+      lg: 1266,
+      xl: 1536,
+    },
+  },
+  direction: 'ltr',
+  mixins: {
+    // layout header settings
+    toolbar: {
+      minHeight: 50,
+      paddingTop: 4,
+      paddingBottom: 4,
+    },
+  },
+  palette,
+  typography,
+};
+
+const theme = createTheme(baseThemeOptions, deDE, coreDeDE);
+const themeWithComponents: Theme = {...theme, components: {...theme.components, ...getComponentOverrides(theme)}}
+
+export default themeWithComponents;
+
+
