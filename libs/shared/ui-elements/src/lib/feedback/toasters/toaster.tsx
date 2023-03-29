6e5b5ba5724446abad494b2811d396de1df26d18
Jose Conchello <jose.conchello@gmail.com>
2023-03-29 13:00:45 -0700

    make style sheets always be the last import

--- libs/shared/ui-elements/src/lib/feedback/toasters/toaster.tsx
+++ libs/shared/ui-elements/src/lib/feedback/toasters/toaster.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { SafetyCheck } from '@mui/icons-material';
+import { Input } from '@mui/material';
 import { Toaster } from 'react-hot-toast';
+
+import { MyType } from './type';
+
+import styles from './toaster.module.css';
+
+type test = Record<string, unknown>;
 
 //TODO: use mui theming
 export const LayoutToaster: React.FC = () => {
+  const test = true;
+
   return (
     <Toaster
       toastOptions={{
---
+++ libs/shared/ui-elements/src/lib/feedback/toasters/type.ts
@@ -0,0 +1,6 @@
+export type MyType = {
+  test: string;
+  testNum: number;
+  bool: boolean;
+  func: (...args: any) => any;
+};
