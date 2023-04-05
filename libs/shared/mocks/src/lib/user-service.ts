6259e96682ff23067fff7b4e820ad41133db68f5
Jose Conchello <jose.conchello@gmail.com>
2023-04-05 14:45:52 -0700

    convert msw files to typescript

--- libs/shared/mocks/src/lib/user-service.ts
+++ libs/shared/mocks/src/lib/user-service.ts
@@ -1,13 +1,14 @@
 import { rest } from "msw";
-import {db} from './database';
-import {RESPONSE_DELAY_MS} from './constants';
+
+import { RESPONSE_DELAY_MS } from './constants';
+import { db } from './database';
 
 
- const handlers = [
+const handlers = [
   rest.get("/api/users", function (req, res, ctx) {
     const data = db.users.getAll();
     return res(ctx.delay(RESPONSE_DELAY_MS), ctx.json(data));
   }),
-  
+
 ];
 export default handlers;
