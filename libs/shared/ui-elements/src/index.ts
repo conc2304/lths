f7e18cb44065b2818b2b2a9318091f9471549c3b
Anantha Boudmanabhan <anantha@sunverasoftware.com>
2023-04-11 10:47:37 +0530

    Cleaned up code

--- libs/shared/ui-elements/src/index.ts
+++ libs/shared/ui-elements/src/index.ts
@@ -1,5 +1,4 @@
 export * from './lib/feedback/toasters';
 export * from './lib/inputs/date-range-selector';
-export * from './lib/custom-table/custom-table';
 export * from './lib/data-display/tables';
 export * from './lib/page-header/page-header';
