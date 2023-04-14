f53bc4595cb84cd2550b9183af3527b0cc88c2ca
Matthew-Sunvera <108888476+Matthew-Sunvera@users.noreply.github.com>
2023-04-14 02:45:32 -0400

    Delete popover.ts

--- libs/shared/themes/src/regatta-bay/overrides/popover.ts
+++
@@ -1,9 +0,0 @@
-import { Components, Theme } from '@mui/material/styles';
-// import type {} from '@mui/x-date-pickers/themeAugmentation';
-
-export default function Popover(theme: Theme): Components {
-  return {
-    MuiPopover: {
-    },
-  };
-}
