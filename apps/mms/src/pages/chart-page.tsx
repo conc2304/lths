a26f5b2331e0d124d10d964823b61127cfce097e
TravisGray1 <travisogray1@gmail.com>
2023-04-18 15:46:16 -0400

    Area chart only

--- apps/mms/src/pages/chart-page.tsx
+++ apps/mms/src/pages/chart-page.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import { Typography, Box } from '@mui/material';
 
 import AreaChartComponent from '../../../../libs/shared/ui-charts/area-chart/area-chart';
-import BarGraph from '../../../../libs/shared/ui-charts/bar-chart/bar-chart';
-import ColumnChart from '../../../../libs/shared/ui-charts/column-chart/column-chart';
-import DonutChart from '../../../../libs/shared/ui-charts/donut-chart/donut-chart';
 //DIFFERENT WAYS TO DEFINE PROPS
 
 //const SamplePage:React.FC<{children:React.ReactNode}> = ({children}):JSX.Element => {
@@ -12,31 +9,6 @@ import DonutChart from '../../../../libs/shared/ui-charts/donut-chart/donut-char
 //const SamplePage = ():JSX.Element => {
 //const SamplePage :React.FC<{}> = ():JSX.Element => {
 //  const SamplePage = (props:any):JSX.Element => {
-
-const data = [
-  {
-    title: 'User Segments & Location',
-    subtitle: 'Who is using the app? And where do they use it?',
-    info: {
-      description: 'This chart shows the distribution of app users by location and user segment.',
-      url: 'https://example.com/user-segments-location',
-    },
-    metrics: [
-      {
-        title: 'Users',
-        description: 'Total number of app users',
-        subtitle: null,
-        data: [
-          { title: 'USA', value: 1000 },
-          { title: 'Europe', value: 500 },
-          { title: 'Asia', value: 250 },
-          { title: 'Australia', value: 150 },
-          { title: 'Other', value: 100 },
-        ],
-      },
-    ],
-  },
-];
 
 const histogramData = {
   // other props can be passed here
@@ -131,27 +103,8 @@ const SamplePage = (): JSX.Element => {
         nostrum exercitation illampu laborings nisi ut liquid ex ea commons construal. Duos aube grue dolor in reprehended in voltage veil esse colum doolie eu
         fujian bulla parian. Exceptive sin ocean cuspidate non president, sunk in culpa qui officiate descent molls anim id est labours.
       </Typography>
-      <Box
-        sx={{
-          width: '100%',
-          height: '400px',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          padding: '16px',
-        }}
-      >
-        <DonutChart data={data} />
-      </Box>
       <Box>
         <AreaChartComponent data={histogramData.data} />
-      </Box>
-
-      <Box>
-        <ColumnChart />
-      </Box>
-      <Box>
-        <BarGraph />
       </Box>
     </Box>
   );
