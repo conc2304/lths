64fe371e3cd087ead50107bd75dddd54b970742e
Jose Conchello <jose.conchello@gmail.com>
2023-04-19 11:37:13 -0700

    refactor date range function to date utils

--- apps/mms/src/fixtures/date-button-group-schema.ts
+++ apps/mms/src/fixtures/date-button-group-schema.ts
@@ -1,172 +1,45 @@
 import {
-  addMonths,
-  addQuarters,
-  eachQuarterOfInterval,
-  endOfDay,
-  endOfMonth,
-  endOfQuarter,
-  endOfYear,
-  getQuarter,
-  isLastDayOfMonth,
-  isSameDay,
-  lastDayOfWeek,
-  lastDayOfYear,
-  nextSaturday,
-  previousSunday,
-  startOfDay,
-  startOfMonth,
-  startOfQuarter,
-  startOfWeek,
-  startOfYear,
-  subDays,
-  subHours,
-  subMonths,
-  subQuarters,
-  subWeeks,
-  subYears,
-} from 'date-fns';
+  getPrevFullDayRange,
+  getPrevFullHalfYearRange,
+  getPrevFullHourRange,
+  getPrevFullMonthRange,
+  getPrevFullQuarterRange,
+  getPrevFullWeekRange,
+  getPrevFullYearRange,
+} from '@lths/shared/utils';
 import { DateFilterOption } from 'libs/shared/ui-elements/src/lib/inputs/date-range-selector/types';
 
 export const ButtonGroupConf: DateFilterOption = [
   {
     label: '1 Hour',
-    dateRangeFn: () => {
-      // WORKS
-      const now = new Date();
-      return { startDate: subHours(now, 1), endDate: now };
-    },
+    dateRangeFn: getPrevFullHourRange,
   },
   {
-    // 12 am - 12am of the previous day
     label: '1 Day',
-    dateRangeFn: () => {
-      // WORKS
-      const now = new Date();
-      const yesterday = subDays(now, 1);
-      const startDate = startOfDay(yesterday);
-      const endDate = endOfDay(yesterday);
-
-      return { startDate, endDate };
-    },
+    dateRangeFn: getPrevFullDayRange,
   },
   {
-    // Previous full week Sunday - Saturday 12 am - 12am
     label: '7 Days',
-    dateRangeFn: () => {
-      // WORKS
-      const now = new Date();
-      const isLastDayOfWeek = isSameDay(now, lastDayOfWeek(now));
-      const prevSunday = previousSunday(subWeeks(now, 1));
-      const followingSaturday = endOfDay(nextSaturday(prevSunday));
-
-      const startDate = isLastDayOfWeek ? startOfWeek(now) : prevSunday;
-      const endDate = isLastDayOfWeek ? now : followingSaturday;
-
-      return { startDate, endDate };
-    },
+    dateRangeFn: getPrevFullWeekRange,
   },
   {
     // Previous full Month
     label: '30 Days',
-    dateRangeFn: () => {
-      // Works
-      const now = new Date();
-      const startDate = isLastDayOfMonth(now) ? startOfMonth(now) : startOfDay(subMonths(startOfMonth(now), 1));
-      const endDate = isLastDayOfMonth(now) ? endOfDay(now) : endOfMonth(startDate);
-
-      return {
-        startDate,
-        endDate,
-      };
-    },
+    dateRangeFn: getPrevFullMonthRange,
   },
   {
     // Previous Quarter
     label: '3 Months',
-    dateRangeFn: () => {
-      // const now = new Date();
-      const now = new Date('06/30/2023');
-
-      const isEndOfQuarter = isSameDay(now, endOfQuarter(now));
-      const startDate = isEndOfQuarter ? startOfQuarter(now) : startOfQuarter(subQuarters(now, 1));
-      const endDate = isEndOfQuarter ? endOfDay(now) : endOfQuarter(subQuarters(now, 1));
-
-      return {
-        startDate,
-        endDate,
-      };
-    },
+    dateRangeFn: getPrevFullQuarterRange,
   },
   {
     // Previous full half year
     label: '6 Months',
-    dateRangeFn: () => {
-      // const now = new Date();
-      const now = new Date('6/30/2023');
-      console.log('N: ', now);
-      console.log('Quarter Interval');
-
-      console.log(eachQuarterOfInterval({ start: subQuarters(startOfYear(now), 2), end: endOfYear(now) }));
-
-      const endQ2 = endOfQuarter(addQuarters(startOfYear(now), 1));
-      const endQ4 = endOfQuarter(addQuarters(startOfYear(now), 3));
-      // check if we are at the end of either q2 or q4
-      const isEndOfQ2 = isSameDay(now, endQ2);
-      const isEndOfQ4 = isSameDay(now, endQ4);
-
-      const isHalfYear = isEndOfQ2 || isEndOfQ4;
-
-      let startDate: Date;
-      let endDate: Date;
-
-      if (isHalfYear) {
-        startDate = startOfQuarter(isEndOfQ2 ? subQuarters(endQ2, 1) : subQuarters(endQ4, 1));
-        endDate = isEndOfQ2 ? endQ2 : endQ4;
-      } else {
-        // find the nearest half year in the past (quarters 2 or 4)
-        const currQuarter = getQuarter(now); // 1,2,3,4
-
-        if ([1, 2].includes(currQuarter)) {
-          // then we want quarters 3-4 of last year
-          endDate = endOfQuarter(subDays(startOfYear(now), 5));
-          startDate = startOfQuarter(subQuarters(endDate, 1));
-        } else if ([3, 4].includes(currQuarter)) {
-          // then we want quarters 1-2 of this year
-          startDate = startOfYear(now);
-          endDate = endQ2;
-        }
-      }
-
-      return {
-        startDate,
-        endDate,
-      };
-    },
+    dateRangeFn: getPrevFullHalfYearRange,
   },
   {
     // Previous full year
     label: '12 Months',
-    dateRangeFn: () => {
-      // const now = new Date();
-      const now = new Date('12/31/2023');
-      console.log('N: ', now);
-      const isEndOfYear = isSameDay(now, lastDayOfYear(now));
-
-      let startDate: Date;
-      let endDate: Date;
-
-      if (isEndOfYear) {
-        startDate = startOfYear(now);
-        endDate = now;
-      } else {
-        startDate = startOfYear(subYears(now, 1));
-        endDate = endOfYear(subYears(now, 1));
-      }
-
-      return {
-        startDate,
-        endDate,
-      };
-    },
+    dateRangeFn: getPrevFullYearRange,
   },
 ];
--- libs/shared/utils/src/lib/date.ts
+++ libs/shared/utils/src/lib/date.ts
@@ -1,3 +1,152 @@
+import {
+  addQuarters,
+  endOfDay,
+  endOfMonth,
+  endOfQuarter,
+  endOfYear,
+  getQuarter,
+  isLastDayOfMonth,
+  isSameDay,
+  lastDayOfWeek,
+  lastDayOfYear,
+  nextSaturday,
+  previousSunday,
+  startOfDay,
+  startOfMonth,
+  startOfQuarter,
+  startOfWeek,
+  startOfYear,
+  subDays,
+  subHours,
+  subMonths,
+  subQuarters,
+  subWeeks,
+  subYears,
+} from 'date-fns';
+
 export const myPlaceholderUtil = (date: Date) => {
   return date.toISOString();
+};
+
+/**
+ * Date Range/Interval Getters
+ */
+
+export const getPrevFullHourRange = (date: Date = new Date()) => {
+  return { startDate: subHours(date, 1), endDate: date };
+};
+
+/**
+ *  @desc 00:00 - 23:59 of the previous day
+ */
+export const getPrevFullDayRange = (date: Date = new Date()) => {
+  const yesterday = subDays(date, 1);
+  const startDate = startOfDay(yesterday);
+  const endDate = endOfDay(yesterday);
+
+  return { startDate, endDate };
+};
+
+/**
+ *  @desc Start and End dateTime Previous full week: Sunday 00:00 - Saturday 23:59
+ */
+export const getPrevFullWeekRange = (date: Date = new Date()) => {
+  // WORKS
+  const isLastDayOfWeek = isSameDay(date, lastDayOfWeek(date));
+  const prevSunday = previousSunday(subWeeks(date, 1));
+  const followingSaturday = endOfDay(nextSaturday(prevSunday));
+
+  const startDate = isLastDayOfWeek ? startOfWeek(date) : prevSunday;
+  const endDate = isLastDayOfWeek ? date : followingSaturday;
+
+  return { startDate, endDate };
+};
+
+/**
+ * @desc Start and End dateTime of previous full month
+ */
+export const getPrevFullMonthRange = (date: Date = new Date()) => {
+  const startDate = isLastDayOfMonth(date) ? startOfMonth(date) : startOfDay(subMonths(startOfMonth(date), 1));
+  const endDate = isLastDayOfMonth(date) ? endOfDay(date) : endOfMonth(startDate);
+
+  return {
+    startDate,
+    endDate,
+  };
+};
+
+/**
+ * @desc Start and End dateTime of previous full quarter. Starts of Jan, April, July, Oct. Ends of March, June, Sept, Dec.
+ */
+export const getPrevFullQuarterRange = (date: Date = new Date()) => {
+  const isEndOfQuarter = isSameDay(date, endOfQuarter(date));
+  const startDate = isEndOfQuarter ? startOfQuarter(date) : startOfQuarter(subQuarters(date, 1));
+  const endDate = isEndOfQuarter ? endOfDay(date) : endOfQuarter(subQuarters(date, 1));
+
+  return {
+    startDate,
+    endDate,
+  };
+};
+
+/**
+ * @desc Start and End dateTime of previous full half year: start of Jan or July to end of June or Dec.
+ */
+export const getPrevFullHalfYearRange = (date: Date = new Date()) => {
+  const endQ2 = endOfQuarter(addQuarters(startOfYear(date), 1));
+  const endQ4 = endOfQuarter(addQuarters(startOfYear(date), 3));
+
+  // check if we are at the end of either q2 or q4
+  const isEndOfQ2 = isSameDay(date, endQ2);
+  const isEndOfQ4 = isSameDay(date, endQ4);
+  const isHalfYear = isEndOfQ2 || isEndOfQ4;
+
+  let startDate: Date;
+  let endDate: Date;
+
+  if (isHalfYear) {
+    startDate = startOfQuarter(isEndOfQ2 ? subQuarters(endQ2, 1) : subQuarters(endQ4, 1));
+    endDate = isEndOfQ2 ? endQ2 : endQ4;
+  } else {
+    // find the nearest half year in the past (quarters 2 or 4)
+    const currQuarter = getQuarter(date); // 1,2,3,4
+
+    if ([1, 2].includes(currQuarter)) {
+      // then we want quarters 3-4 of last year
+      endDate = endOfQuarter(subDays(startOfYear(date), 5));
+      startDate = startOfQuarter(subQuarters(endDate, 1));
+    } else {
+      // then we want quarters 1-2 of this year
+      startDate = startOfYear(date);
+      endDate = endQ2;
+    }
+  }
+
+  return {
+    startDate,
+    endDate,
+  };
+};
+
+/**
+ * @desc Start and End dateTime of previous full year start of Jan to end of Dec
+ */
+export const getPrevFullYearRange = (date: Date = new Date()) => {
+  const isEndOfYear = isSameDay(date, lastDayOfYear(date));
+
+  let startDate: Date;
+  let endDate: Date;
+
+  if (isEndOfYear) {
+    startDate = startOfYear(date);
+    endDate = date;
+  } else {
+    startDate = startOfYear(subYears(date, 1));
+    endDate = endOfYear(subYears(date, 1));
+  }
+
+  return {
+    startDate,
+    endDate,
+  };
 };
