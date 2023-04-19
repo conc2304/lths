6069314c5bb9758177feaa58753293a23d4541c8
Prem Dattan <123981166+CodingKwazii@users.noreply.github.com>
2023-04-18 22:55:30 -0700

    wip mocks

--- libs/shared/mocks/src/lib/mocked-path-registry.ts
+++
@@ -1,6 +0,0 @@
-import { MSWPathConf } from './types';
-import insightsMockInterceptors from '../lib/modules/insights';
-
-const mockedPaths: MSWPathConf[] = [...insightsMockInterceptors];
-
-export default mockedPaths;
