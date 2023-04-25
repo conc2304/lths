db2b9079d5cf8ceae26097b4dcf3c4c3754e044a
“Sahithi <skotagiri@briteliteimmersive.com>
2023-04-25 13:06:49 -0500

    added stubs for histogramdata cardkpi and pagepreview

--- libs/shared/mocks/src/lib/services/insights/overview/index.ts
+++ libs/shared/mocks/src/lib/services/insights/overview/index.ts
@@ -1,5 +1,8 @@
+import OverviewCardKpiPayload from './cardkpi.stub';
 import OverviewHistogramPayload from './histogram.stub';
+import OverviewHistogramDataPayload from './histogramdata.stub';
 import OverviewKpiPayload from './kpi.stub';
+import OverviewPagePreviewPayload from './pagepreview.stub';
 import OverviewSegmentationPayload from './segmentation.sub';
 import OverviewTabularPayload from './tabular.sub';
 import { getSuccessfulResponse } from '../../api';
@@ -9,6 +12,9 @@ const responses = [
   getSuccessfulResponse('/insights/overview/histogram', OverviewHistogramPayload),
   getSuccessfulResponse('/insights/overview/segmentation', OverviewSegmentationPayload),
   getSuccessfulResponse('/insights/overview/tabular', OverviewTabularPayload),
+  getSuccessfulResponse('/insights/overview/cardkpi', OverviewCardKpiPayload),
+  getSuccessfulResponse('/insights/overview/pagepreview', OverviewPagePreviewPayload),
+  getSuccessfulResponse('/insights/overview/histogramdata', OverviewHistogramDataPayload),
 ];
 
 export default responses;
