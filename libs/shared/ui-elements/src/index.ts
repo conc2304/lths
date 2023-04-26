a7358d24e82bfa68f0869b55b8ccd8ff19f82f9b
Prem Dattan <123981166+CodingKwazii@users.noreply.github.com>
2023-04-26 12:16:00 -0700

    fix!: removing ref of deleted comp. Missed during pr merge.

--- libs/shared/ui-elements/src/index.ts
+++ libs/shared/ui-elements/src/index.ts
@@ -1,11 +1,8 @@
-import InfoTooltip from './lib/data-display/icons/tooltip/info-tooltip';
-
 export * from './lib/inputs';
 export * from './lib/feedback/toasters';
 export * from './lib/inputs/date-range-selector';
 export * from './lib/data-display/cards/basic';
 export * from './lib/data-display/tables';
-export * from './lib/page-header/page-header';
 
 export * from './lib/layouts/stacks';
 
