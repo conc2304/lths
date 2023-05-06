151a0f08d8c46ef072d76c3603f0cf9022db98c2
Prem Dattan <123981166+CodingKwazii@users.noreply.github.com>
2023-05-05 18:19:01 -0700

    fix: CSP issue - public api test

--- apps/mms/src/routes/configs.tsx
+++ apps/mms/src/routes/configs.tsx
@@ -1,6 +1,7 @@
 import { lazy } from 'react';
-import { LazyLoader } from '@lths/shared/ui-layouts';
 import { Navigate } from 'react-router-dom';
+
+import { LazyLoader } from '@lths/shared/ui-layouts';
 
 import { PrivateLayout, PublicLayout } from './layouts';
 import { generateRouteConfig } from './transformer';
@@ -8,7 +9,7 @@ import pages from '../pages/paths';
 
 // eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
 const LoginPage = LazyLoader(lazy(() => import('libs/shared/ui-login/src/lib/login')));
-
+const PubliApiTestPage = LazyLoader(lazy(() => import('../pages/demo/public-page')));
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const AuthenticationRoutes = (authenticated: boolean) => {
   return {
@@ -18,6 +19,10 @@ export const AuthenticationRoutes = (authenticated: boolean) => {
       {
         path: '/login',
         element: <LoginPage />,
+      },
+      {
+        path: '/public-api-test',
+        element: <PubliApiTestPage />,
       },
     ],
   };
