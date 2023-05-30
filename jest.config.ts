edb8d77738471b43c3d8c979bf89ac24ffb79765
Jose Conchello <conc2304@users.noreply.github.com>
2023-05-30 13:58:14 -0400

    Update/jest configs (#86)
    * fine tune the coverageFrom for jest coverage reports
    
    * update coverage from
    
    * remove commented out line

--- jest.config.ts
+++ jest.config.ts
@@ -2,4 +2,12 @@ import { getJestProjects } from '@nx/jest';
 
 export default {
   projects: getJestProjects(),
+  collectCoverageFrom: [
+    '<rootDir>/**/src/**/*.@(js|jsx|ts|tsx)',
+    // Not Part Of Coverage
+    '!<rootDir>/**/src/**/constant?(s).ts',
+    '!<rootDir>/**/src/**/index.ts',
+    '!<rootDir>/**/src/**/*.@(e2e|cy|stories).(ts|tsx|js|jsx)',
+    '!**/mockServiceWorker.js',
+  ],
 };
