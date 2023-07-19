562550c820d78be97401f7e9677f33a19bea2dc6
saranya122000-s <saranya@sunverasoftware.com>
2023-07-19 15:55:31 +0530

    Removed Mock in editor page

--- libs/shared/mocks/src/lib/services/pages/index.ts
+++ libs/shared/mocks/src/lib/services/pages/index.ts
@@ -1,11 +1,10 @@
 import Generic, { Custom } from '././editor';
 import Images from './images';
 import DefaultPages from './pages/default-pages';
-import PageDetail from './editor/page-detail';
 import { MSWPathConf } from '../types';
 
 const generics: MSWPathConf[] = [...Generic];
 
-export const CustomServices = [...Custom, Images, DefaultPages,PageDetail];
+export const CustomServices = [...Custom, Images, DefaultPages];
 
 export default generics;
