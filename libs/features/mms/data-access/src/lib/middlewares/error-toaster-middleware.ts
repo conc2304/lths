c6bf1f5236cc1f336fd206565b3bcbc6fc9afa28
Anantha Boudmanabhan <anantha@sunverasoftware.com>
2023-08-02 19:11:20 +0530

    Fixed toast error as per new response format

--- libs/features/mms/data-access/src/lib/middlewares/error-toaster-middleware.ts
+++ libs/features/mms/data-access/src/lib/middlewares/error-toaster-middleware.ts
@@ -1,13 +1,14 @@
 import { isRejectedWithValue } from '@reduxjs/toolkit';
-
-import toast from "react-hot-toast";
+import toast from 'react-hot-toast';
 
 export const errorToasterMiddleware = (api) => (next) => (action) => {
   // RTK Query uses `createAsyncThunk` from RTK under the hood, so we can use these to match the action creators!
   if (isRejectedWithValue(action)) {
-    const error =action.payload?.data?.error || 'Something went wrong. Please try logging in again.';
+    const error = action.payload?.data?.error
+      ? action.payload?.data?.message
+      : 'Something went wrong. Please try logging in again.';
     toast.error(error);
-    console.warn("ERROR", action);
+    console.warn('ERROR', action);
   }
   return next(action);
 };
