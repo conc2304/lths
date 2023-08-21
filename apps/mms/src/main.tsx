f6316a51a003f7f92a0b34cfac8497a127c03b24
Anantha Boudmanabhan <104918864+anantha-t@users.noreply.github.com>
2023-08-21 21:54:14 +0530

    Added robot as the font family (#168)

--- apps/mms/src/main.tsx
+++ apps/mms/src/main.tsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react';
+import '@fontsource/roboto';
 import * as ReactDOM from 'react-dom/client';
 
 import App from './app';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <StrictMode>
     <App />
