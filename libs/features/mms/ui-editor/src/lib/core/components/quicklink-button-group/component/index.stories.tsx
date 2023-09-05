c4f627c308cac9d3831630868ce25c7c86654c54
saranya122000-s <saranya@sunverasoftware.com>
2023-09-05 15:48:21 +0530

    component and toolbar storybook for SocialIconButton

--- libs/features/mms/ui-editor/src/lib/core/components/quicklink-button-group/component/index.stories.tsx
+++ libs/features/mms/ui-editor/src/lib/core/components/quicklink-button-group/component/index.stories.tsx
@@ -1,8 +1,10 @@
+import { Box } from '@mui/material';
+
 import QuicklinkButtonGroupComponent from './index';
-import type { Meta, StoryFn } from '@storybook/react';
 import mockComponent from '../../../../context/mockdata';
 import { Component } from '../../enum';
-import { Box } from '@mui/material';
+
+import type { Meta, StoryFn } from '@storybook/react';
 
 const Story: Meta<typeof QuicklinkButtonGroupComponent> = {
   component: QuicklinkButtonGroupComponent,
@@ -11,56 +13,56 @@ const Story: Meta<typeof QuicklinkButtonGroupComponent> = {
 export default Story;
 
 const Template: StoryFn<typeof QuicklinkButtonGroupComponent> = (args) => (
-    <Box 
-        sx={{
-            backgroundColor: 'rgb(245, 245, 245)',
-            display: 'flex', justifyContent: 'center',
-            alignItems: 'center' 
-        }}
-    >
-        <Box sx={{ width: '375px', backgroundColor: 'white' }}>
-            <QuicklinkButtonGroupComponent {...args} />
-        </Box>
+  <Box
+    sx={{
+      backgroundColor: 'rgb(245, 245, 245)',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}
+  >
+    <Box sx={{ width: '375px', backgroundColor: 'white' }}>
+      <QuicklinkButtonGroupComponent {...args} />
     </Box>
+  </Box>
 );
-
 
 export const Primary = Template.bind({});
 Primary.args = {
-    ...mockComponent,
-    __ui_id__ : "3333333",
-    component_id: Component.QuicklinkButtonGroup,
-    properties_data: {    
-        first_button: {
-          label: "Medical Help",
-          icon: "https://i.im.ge/2022/12/05/S82BeW.Group.png",
-          action: {
-            type: '',
-            page_id: 'medical page',
-            page_link: 'first aid link',
-          },
-        },
-        second_button: {
-          label: "Report",
-          icon: "https://i.im.ge/2022/12/05/S824gr.Group.png",
-          action: {
-            type: '',
-            page_id: 'report crime',
-            page_link: 'local police department link',
-          },
-        },
-    }
-}
+  ...mockComponent,
+  __ui_id__: '3333333',
+  component_id: Component.QuicklinkButtonGroup,
+  properties_data: {
+    first_button: {
+      label: 'Medical Help',
+      icon: 'https://i.im.ge/2022/12/05/S82BeW.Group.png',
+      action: {
+        type: '',
+        page_id: 'medical page',
+        page_link: 'first aid link',
+      },
+    },
+    second_button: {
+      label: 'Report',
+      icon: 'https://i.im.ge/2022/12/05/S824gr.Group.png',
+      action: {
+        type: '',
+        page_id: 'report crime',
+        page_link: 'local police department link',
+      },
+    },
+  },
+};
 
 Primary.argTypes = {
-    __ui_id__: { table: { disable: true}},
-    component_id: { table: { disable: true}},
-    _id: { table: { disable: true}},
-    name: { table: { disable: true}},
-    description: { table: { disable: true}},
-    category: { table: { disable: true}},
-    image_url: { table: { disable: true}},
-    display_order: { table: { disable: true}},
-    variation_id: { table: { disable: true}},
-    schema: { table: { disable: true}},
-}
+  __ui_id__: { table: { disable: true } },
+  component_id: { table: { disable: true } },
+  _id: { table: { disable: true } },
+  name: { table: { disable: true } },
+  description: { table: { disable: true } },
+  category: { table: { disable: true } },
+  image_url: { table: { disable: true } },
+  display_order: { table: { disable: true } },
+  variation_id: { table: { disable: true } },
+  schema: { table: { disable: true } },
+};
