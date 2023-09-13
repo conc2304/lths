338120b25c555c7800c781fd01bef95bd1494331
saranya122000-s <saranya@sunverasoftware.com>
2023-09-13 12:17:09 +0530

    duplicate import removed

--- libs/features/mms/ui-editor/src/lib/core/index.ts
+++ libs/features/mms/ui-editor/src/lib/core/index.ts
@@ -1,4 +1,3 @@
 export * from './containers';
-export { Component } from './components/enum';
 export type { AutocompleteItemProps } from './components/types';
 export { Component } from './components/enum';
