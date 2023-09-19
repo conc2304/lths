9253dea2e167a6c79307b18f64f1bb8752220495
saranya122000-s <saranya@sunverasoftware.com>
2023-09-19 19:06:19 +0530

    styling changes done in component and toolbar

--- libs/features/mms/ui-editor/src/lib/core/components/text-with-icon/component/index.tsx
+++ libs/features/mms/ui-editor/src/lib/core/components/text-with-icon/component/index.tsx
@@ -8,18 +8,10 @@ const TextwithIcon = (props: TextWithIconProps) => {
     data: { icon, title },
     __ui_id__: id,
   } = props;
-  console.log('textWithIcon', props);
   return (
-    <BasicContainer id={id} sx={{ display: 'flex', flexDirection: 'row' }}>
-      <Avatar alt="Icon" src={icon} sx={{ width: 20, height: 20, marginRight: 1 }} />
-      <Typography
-        variant="subtitle1"
-        gutterBottom
-        color={'#ABABAC'}
-        fontSize={'14px'}
-        fontWeight={400}
-        lineHeight={'20px'}
-      >
+    <BasicContainer id={id} sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+      <Avatar alt="Icon" src={icon} sx={{ width: 20, height: 20, marginRight: 2, fontSize: '14px' }} />
+      <Typography variant="subtitle1" gutterBottom color={'#ABABAC'} fontSize={'14px'} fontWeight={400}>
         {title}
       </Typography>
     </BasicContainer>
--- libs/features/mms/ui-editor/src/lib/core/components/text-with-icon/toolbar/index.tsx
+++ libs/features/mms/ui-editor/src/lib/core/components/text-with-icon/toolbar/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-import { BasicTextField, ImageAutocomplete, AutocompleteOptionProps } from '../../../../elements';
+import { OutlinedTextField, ImageAutocomplete, AutocompleteOptionProps, GroupLabel } from '../../../../elements';
 import { ToolContainer } from '../../../../elements/containers';
 import { useToolbarChange } from '../../hooks';
 import { TextWithIconProps } from '../../types';
@@ -24,20 +24,19 @@ const TextwithIconToolbar = (props: TextWithIconProps) => {
 
   const { handleTitleChange, updateComponentProp } = useToolbarChange();
   const handleIconChange = (value: string) => {
-    console.log('value,index', value);
     updateComponentProp('icon', value);
   };
 
   return (
     <ToolContainer key={id} aria-label={'TextWithIcon Toolbar'}>
-      <BasicTextField label={'Title'} value={title} onChange={handleTitleChange} />
+      <GroupLabel label={'Text With Icon'} />
+      <OutlinedTextField label={'Title'} value={title} onChange={handleTitleChange} />
       <ImageAutocomplete
         aria-label="Icon"
         label="Icon"
         value={icon}
         data={icons}
         onChange={(value) => {
-          console.log('eee', value);
           return handleIconChange(value);
         }}
       />
