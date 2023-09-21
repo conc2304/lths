f6e6c46475dc7e39eb3dfbfeb45b17122a16245d
saranya122000-s <saranya@sunverasoftware.com>
2023-09-21 15:34:35 +0530

    styling changes fixed

--- libs/features/mms/ui-editor/src/lib/core/components/text-with-icon/component/index.tsx
+++ libs/features/mms/ui-editor/src/lib/core/components/text-with-icon/component/index.tsx
@@ -1,4 +1,4 @@
-import { Typography, Avatar, Box } from '@mui/material';
+import { Typography, Avatar, Stack } from '@mui/material';
 
 import colors from '../../../../common/colors';
 import { BasicContainer } from '../../../../elements';
@@ -11,12 +11,12 @@ const TextwithIcon = (props: TextWithIconProps) => {
   } = props;
   return (
     <BasicContainer id={id}>
-      <Box sx={{ display: 'flex', flexDirection: 'row', textAlign: 'center', paddingTop: '4px' }}>
-        <Avatar alt="Icon" src={icon} sx={{ width: 14, height: 17, marginRight: 2, marginTop: '2px' }} />
-        <Typography variant="subtitle1" gutterBottom color={colors.typography.color} fontSize={'14px'} fontWeight={400}>
+      <Stack direction="row" paddingY={1} alignItems="center" spacing={0.5}>
+        <Avatar alt="Icon" src={icon} sx={{ width: 14, height: 14 }} />
+        <Typography variant="subtitle1" color={colors.typography.color} fontSize={'0.875rem'} fontWeight={400}>
           {title}
         </Typography>
-      </Box>
+      </Stack>
     </BasicContainer>
   );
 };
