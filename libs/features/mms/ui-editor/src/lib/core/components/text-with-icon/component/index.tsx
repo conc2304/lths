a54ee3ebbbc965c88150cb50a3bfb3a89622dd1d
saranya122000-s <saranya@sunverasoftware.com>
2023-09-21 13:13:26 +0530

    changed color and fixes styling

--- libs/features/mms/ui-editor/src/lib/core/components/text-with-icon/component/index.tsx
+++ libs/features/mms/ui-editor/src/lib/core/components/text-with-icon/component/index.tsx
@@ -1,5 +1,6 @@
 import { Typography, Avatar, Box } from '@mui/material';
 
+import colors from '../../../../common/colors';
 import { BasicContainer } from '../../../../elements';
 import { TextWithIconProps } from '../../types';
 
@@ -10,9 +11,9 @@ const TextwithIcon = (props: TextWithIconProps) => {
   } = props;
   return (
     <BasicContainer id={id}>
-      <Box sx={{ display: 'flex', flexDirection: 'row', textAlign: 'center', paddingTop: '8px' }}>
-        <Avatar alt="Icon" src={icon} sx={{ width: 20, height: 20, marginRight: 2, fontSize: '14px' }} />
-        <Typography variant="subtitle1" gutterBottom color={'#ABABAC'} fontSize={'14px'} fontWeight={400}>
+      <Box sx={{ display: 'flex', flexDirection: 'row', textAlign: 'center', paddingTop: '4px' }}>
+        <Avatar alt="Icon" src={icon} sx={{ width: 14, height: 17, marginRight: 2, marginTop: '2px' }} />
+        <Typography variant="subtitle1" gutterBottom color={colors.typography.color} fontSize={'14px'} fontWeight={400}>
           {title}
         </Typography>
       </Box>
