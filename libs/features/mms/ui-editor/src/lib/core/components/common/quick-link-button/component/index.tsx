a7c663b816911eb841ea6f4c3a39e3f198786c64
saranya122000-s <saranya@sunverasoftware.com>
2023-09-23 09:59:05 +0530

    Given alt name for Image element

--- libs/features/mms/ui-editor/src/lib/core/components/common/quick-link-button/component/index.tsx
+++ libs/features/mms/ui-editor/src/lib/core/components/common/quick-link-button/component/index.tsx
@@ -15,7 +15,7 @@ const QuickLinkButtonComponent = ({ title = 'link', icon }: { title: string; ico
       }}
     >
       <Stack direction="column" alignItems="center" spacing={1}>
-        {icon && <img src={icon} alt={''} style={{ width: 24, height: 24 }} />}
+        {icon && <img src={icon} alt={'Icon'} style={{ width: 24, height: 24 }} />}
         <Typography sx={{ letterSpacing: '0.16px', fontSize: 14, color: '#D8BA90', textTransform: 'uppercase' }}>
           {title !== 'link' && title}
         </Typography>
