732cf6bc5f06748daecc6ef40af470575dd30bc4
Sahithi Kotagiri <116521679+skotagiri08@users.noreply.github.com>
2023-10-04 10:19:07 -0500

    fix: page table new style implementation (#270)
    Co-authored-by: “Sahithi <“skotagiri@briteliteimmersive.com”>

--- libs/shared/themes/src/regatta-bay/overrides/table.ts
+++ libs/shared/themes/src/regatta-bay/overrides/table.ts
@@ -1,16 +1,14 @@
-import {Theme} from '@mui/material'
+import { Theme } from '@mui/material';
 import { Components } from '@mui/material/styles';
 
 export default function Table(theme: Theme): Components {
-    return {
-        MuiTableBody: {
-            styleOverrides: {
-                root: {
-                    '& .MuiTableRow-root:nth-of-type(even)': {
-                        backgroundColor: theme.palette.secondary.main,
-                      },
-                }
-            }
-        }
-    }
+  return {
+    MuiTableCell: {
+      styleOverrides: {
+        root: {
+          padding: theme.spacing(1.5),
+        },
+      },
+    },
+  };
 }
