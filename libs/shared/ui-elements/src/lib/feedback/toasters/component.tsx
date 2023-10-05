ffdaee358e8a5240d1e1d0015359f8bad2c1673b
Jose Conchello <conc2304@users.noreply.github.com>
2023-10-05 16:03:25 -0700

    fix: handle duplicate api errors MMS-332 (#277)
    * prevent dupes by using an id for the errors we dont want to duplicate
    
    * use mui theme colors
    
    * hash the message to create a unique id
    
    * update comments for hashing
    
    * only add message hash if id is not undefined

--- libs/shared/ui-elements/src/lib/feedback/toasters/component.tsx
+++ libs/shared/ui-elements/src/lib/feedback/toasters/component.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
+import { useTheme } from '@mui/material';
 import { Toaster } from 'react-hot-toast';
 
-//TODO: use mui theming
 export const LayoutToaster: React.FC = () => {
+  const theme = useTheme();
   return (
     <Toaster
       toastOptions={{
         style: { fontWeight: 'bold' },
         success: {
           style: {
-            background: 'green',
-            color: 'white',
+            background: theme.palette.success.main,
+            color: theme.palette.success.contrastText,
           },
         },
         error: {
           style: {
-            background: 'red',
-            color: 'white',
+            background: theme.palette.error.main,
+            color: theme.palette.error.contrastText,
           },
         },
       }}
