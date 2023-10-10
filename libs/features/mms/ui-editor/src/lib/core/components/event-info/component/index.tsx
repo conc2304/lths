49cafcdd33782111c9dd735afea0664334d80b07
saranya122000-s <saranya@sunverasoftware.com>
2023-10-10 14:48:51 +0530

    styling fixed

--- libs/features/mms/ui-editor/src/lib/core/components/event-info/component/index.tsx
+++ libs/features/mms/ui-editor/src/lib/core/components/event-info/component/index.tsx
@@ -1,4 +1,4 @@
-import { Stack, Typography, IconButton } from '@mui/material';
+import { Typography, IconButton, Box } from '@mui/material';
 
 import { BasicContainer } from '../../../../elements';
 import { EventInfoComponentProps } from '../../types';
@@ -11,20 +11,18 @@ const EventInfoComponent = (props: EventInfoComponentProps) => {
 
   return (
     <BasicContainer id={id}>
-      <Stack flexDirection={'column'} justifyContent={'flex-start'} alignItems="flex-start" spacing={0.5}>
-        <Stack direction="row" justifyContent="flex-start" alignItems="center" spacing={2}>
-          <IconButton sx={{ padding: 0 }}>
-            <img src={location_icon} alt={'Icon'} style={{ width: 24, height: 24 }} />
-          </IconButton>
-          <Typography sx={{ fontWeight: 500, fontSize: 16, color: '#FFF' }}>{location_text}</Typography>
-        </Stack>
-        <Stack direction="row" justifyContent="flex-start" alignItems="center" spacing={2}>
-          <IconButton sx={{ padding: 0 }}>
-            <img src={time_icon} alt={'Icon'} style={{ width: 24, height: 24 }} />
-          </IconButton>
-          <Typography sx={{ fontWeight: 500, fontSize: 16, color: '#FFF' }}>{time_text}</Typography>
-        </Stack>
-      </Stack>
+      <Box display={'flex'} flexBasis={'row'} justifyContent={'flex-start'} paddingY={'8px'}>
+        <IconButton sx={{ padding: 0, marginRight: '16px' }}>
+          <img src={location_icon} alt={'Icon'} style={{ width: 24, height: 24 }} />
+        </IconButton>
+        <Typography sx={{ fontWeight: 500, fontSize: 16, color: '#FFF' }}>{location_text}</Typography>
+      </Box>
+      <Box display={'flex'} flexBasis={'row'} justifyContent={'flex-start'} paddingY={'8px'}>
+        <IconButton sx={{ padding: 0, marginRight: '16px' }}>
+          <img src={time_icon} alt={'Icon'} style={{ width: 24, height: 24 }} />
+        </IconButton>
+        <Typography sx={{ fontWeight: 500, fontSize: 16, color: '#FFF' }}>{time_text}</Typography>
+      </Box>
     </BasicContainer>
   );
 };
