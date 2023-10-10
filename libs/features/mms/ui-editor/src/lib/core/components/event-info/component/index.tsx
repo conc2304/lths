5709b040480ac2d95dbfb91382a459fbedca02a9
saranya122000-s <saranya@sunverasoftware.com>
2023-10-10 14:52:51 +0530

    styling fixed

--- libs/features/mms/ui-editor/src/lib/core/components/event-info/component/index.tsx
+++ libs/features/mms/ui-editor/src/lib/core/components/event-info/component/index.tsx
@@ -15,13 +15,13 @@ const EventInfoComponent = (props: EventInfoComponentProps) => {
         <IconButton sx={{ padding: 0, marginRight: '16px' }}>
           <img src={location_icon} alt={'Icon'} style={{ width: 24, height: 24 }} />
         </IconButton>
-        <Typography sx={{ fontWeight: 500, fontSize: 16, color: '#FFF' }}>{location_text}</Typography>
+        <Typography sx={{ fontWeight: 500, fontSize: '1rem', color: '#FFF' }}>{location_text}</Typography>
       </Box>
       <Box display={'flex'} flexBasis={'row'} justifyContent={'flex-start'} paddingY={'8px'}>
         <IconButton sx={{ padding: 0, marginRight: '16px' }}>
           <img src={time_icon} alt={'Icon'} style={{ width: 24, height: 24 }} />
         </IconButton>
-        <Typography sx={{ fontWeight: 500, fontSize: 16, color: '#FFF' }}>{time_text}</Typography>
+        <Typography sx={{ fontWeight: 500, fontSize: '1rem', color: '#FFF' }}>{time_text}</Typography>
       </Box>
     </BasicContainer>
   );
