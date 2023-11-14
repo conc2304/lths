60e605661e3613993e311ef8401190ec9be1528b
Anantha Boudmanabhan <104918864+anantha-t@users.noreply.github.com>
2023-11-14 22:46:11 +0530

    Constraints tab for default page is hidden (#313) MMS-413

--- apps/mms/src/pages/pages/editor/index.tsx
+++ apps/mms/src/pages/pages/editor/index.tsx
@@ -99,6 +99,8 @@ export function PageEditorTabs() {
   //fetch params
   const page_data = data as PageDetail;
 
+  const isVariantPage = page_data && page_data.default_page_id;
+
   // Breadcrumbs title
   const { setPageTitle } = useLayoutActions();
 
@@ -297,7 +299,7 @@ export function PageEditorTabs() {
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
         <Tabs value={currentTab} onChange={handleTabChange}>
           <Tab label={TabItems.page_design.label} value={TabItems.page_design.value} />
-          <Tab label={TabItems.constraints.label} value={TabItems.constraints.value} />
+          {isVariantPage && <Tab label={TabItems.constraints.label} value={TabItems.constraints.value} />}
           <Tab label={TabItems.settings.label} value={TabItems.settings.value} />
         </Tabs>
       </Box>
@@ -312,9 +314,12 @@ export function PageEditorTabs() {
           />
           <AssetsModal open={imageModalOpen} onClose={handleCloseImageModal} onSelect={handleSelectImage} />
         </TabPanel>
-        <TabPanel value={TabItems.constraints.value} currentTab={currentTab}>
-          <PageConstraints />
-        </TabPanel>
+        {isVariantPage && (
+          <TabPanel value={TabItems.constraints.value} currentTab={currentTab}>
+            <PageConstraints />
+          </TabPanel>
+        )}
+
         <TabPanel value={TabItems.settings.value} currentTab={currentTab}>
           <PageSettings data={page_data} onUpdateSettings={updateExtended} />
         </TabPanel>
