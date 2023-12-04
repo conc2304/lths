83589a18cb2da3b526b49c883e08e446ebfb9092
Matthew-Sunvera <mathew@sunverasoftware.com>
2023-12-04 12:43:03 -0600

    fix lint

--- apps/mms/src/components/pages/editor/assets/modal.tsx
+++ apps/mms/src/components/pages/editor/assets/modal.tsx
@@ -13,7 +13,6 @@ import AddIcon from '@mui/icons-material/Add';
 import CloseIcon from '@mui/icons-material/Close';
 
 import { AssetSearchBar } from '@lths/features/mms/ui-components';
-
 import { Table } from '@lths/shared/ui-elements';
 
 import { TableFileInfoRow } from './table-row';
