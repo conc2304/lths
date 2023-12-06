6ec506db669a3a74d3fdc11a96044c8aa09597fe
saranya122000-s <saranya@sunverasoftware.com>
2023-12-06 16:24:29 +0530

    Styling fixed as per Figma

--- libs/features/mms/ui-editor/src/lib/core/components/hero-event/component/index.tsx
+++ libs/features/mms/ui-editor/src/lib/core/components/hero-event/component/index.tsx
@@ -12,14 +12,22 @@ const HeroEventComponent = (props: HeroEventComponentProps) => {
     <BasicContainer id={id}>
       <Box>
         <CardMedia component="img" height="192" image={placeholder} alt={'placeholder'} sx={{ borderRadius: 2.5 }} />
-        <Typography sx={{ color: colors.editor.text, fontWeight: 450, textAlign: 'center' }} marginTop={1.5}>
-          Title
-        </Typography>
-        <Grid container justifyContent={'center'} alignItems={'center'} gap={0.5}>
-          <Typography sx={{ color: colors.editor.subText }}>Sat,Jan 10</Typography>
-          <FiberManualRecordIcon sx={{ color: colors.editor.subText, fontSize: '0.5rem' }} />
-          <Typography sx={{ color: colors.editor.subText }}>7:00PM</Typography>
-        </Grid>
+        <Box paddingY={'0.75rem'}>
+          <Typography sx={{ color: colors.editor.text, fontWeight: 450, textAlign: 'center' }} marginTop={1.5}>
+            Title
+          </Typography>
+          <Grid container justifyContent={'center'} alignItems={'center'} gap={0.5}>
+            <Typography sx={{ color: colors.editor.subText, fontSize: '0.875rem', lineHeight: '1.25rem' }}>
+              Sat,Jan 10
+            </Typography>
+            <FiberManualRecordIcon
+              sx={{ color: colors.editor.subText, fontSize: '0.5rem', width: '0.25rem', height: '0.25rem' }}
+            />
+            <Typography sx={{ color: colors.editor.subText, fontSize: '0.875rem', lineHeight: '1.25rem' }}>
+              7:00PM
+            </Typography>
+          </Grid>
+        </Box>
       </Box>
     </BasicContainer>
   );
