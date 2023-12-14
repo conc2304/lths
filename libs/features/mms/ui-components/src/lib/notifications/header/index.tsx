d953419abf862f2cb72c91f76b7ba58a9140f83e
Prem Dattan <123981166+codingkwazii@users.noreply.github.com>
2023-12-13 18:40:43 -0800

    fix: enable notification push (#369) MMS-541

--- libs/features/mms/ui-components/src/lib/notifications/header/index.tsx
+++ libs/features/mms/ui-components/src/lib/notifications/header/index.tsx
@@ -24,7 +24,7 @@ export const NotificationHeader = ({ onStatusChange, onActionClick, title = 'Not
       id: NotificationStatusProps.SENT,
       label: 'PUSH NOW',
       action: setNotificationStatusSent,
-      isDisabled: true,
+      isDisabled: false,
     },
   ];
 
