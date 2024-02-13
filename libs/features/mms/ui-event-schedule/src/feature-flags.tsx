5d8a0192b1cb438bbb065226c635635c7873a7ef
Jose Conchello <jose.conchello@gmail.com>
2024-02-13 12:14:04 -0800

    update values

--- libs/features/mms/ui-event-schedule/src/feature-flags.tsx
+++ libs/features/mms/ui-event-schedule/src/feature-flags.tsx
@@ -1,35 +1,6 @@
-import { slugify } from '@lths/shared/utils';
-
-const appPrefix = 'MMS';
-const featureModule = 'SCHEDULER';
-
-type GenerateIdProps = { title: string; module: string; appPrefix?: string };
-const generateId = ({ title, module, appPrefix = 'MMS' }: GenerateIdProps) => {
-  return `${appPrefix.toUpperCase()}_${slugify(module).toUpperCase()}--${slugify(title)}`;
-};
-
-export const EVENT_SCHEDULER_IMPORT_EVENTS_FLAG = generateId({
-  title: 'import events',
-  module: featureModule,
-  appPrefix,
-});
-export const EVENT_SCHEDULER_EXPORT_EVENTS_FLAG = generateId({
-  title: 'export events',
-  module: featureModule,
-  appPrefix,
-});
-export const EVENT_SCHEDULER_CREATE_EVENTS_FLAG = generateId({
-  title: 'create events',
-  module: featureModule,
-  appPrefix,
-});
-export const EVENT_SCHEDULER_UPDATE_EVENTS_FLAG = generateId({
-  title: 'update events',
-  module: featureModule,
-  appPrefix,
-});
-export const EVENT_SCHEDULER_UPDATE_EVENT_STATES_FLAG = generateId({
-  title: 'update event-states',
-  module: featureModule,
-  appPrefix,
-});
+export const EVENT_SCHEDULER_IMPORT_EVENTS_FLAG = 'MMS_SCHEDULER--import-events';
+export const EVENT_SCHEDULER_EXPORT_EVENTS_FLAG = 'MMS_SCHEDULER--export-events';
+export const EVENT_SCHEDULER_CREATE_EVENTS_FLAG = 'MMS_SCHEDULER--create-events';
+export const EVENT_SCHEDULER_UPDATE_EVENTS_FLAG = 'MMS_SCHEDULER--update-events';
+export const EVENT_SCHEDULER_UPDATE_EVENT_STATES_FLAG = 'MMS_SCHEDULER--update-event-states';
+export const EVENT_SCHEDULER_BANANA_TEST = 'MMS_SCHEDULER--banana-test ';
