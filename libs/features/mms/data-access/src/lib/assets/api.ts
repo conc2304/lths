e21b29c75ceea5b482ef2fb0cac0ef61d5736305
Jose Conchello <jose.conchello@gmail.com>
2024-03-04 13:50:01 -0800

    i add new api infrastructure for blob calls

--- libs/features/mms/data-access/src/lib/assets/api.ts
+++ libs/features/mms/data-access/src/lib/assets/api.ts
@@ -8,7 +8,7 @@ import {
   UpdateAssetResponse,
   CreateAssetResponse,
 } from './types';
-import { getAddAssetUrl, getAssetsUrl, getUpdateAssetUrl } from './urls';
+import { getAddAssetUrl, getAssetsUrl, getSecureCloudUploadUrl, getUpdateAssetUrl } from './urls';
 
 const createAssetQuery = (request: AssetsRequestProps) => {
   const searchText = request?.queryString || undefined;
@@ -18,6 +18,7 @@ const createAssetQuery = (request: AssetsRequestProps) => {
     body: {
       // dont send querystring if its empty
       queryString: searchText && searchText !== '' ? { search: searchText } : undefined,
+      // on new upload force that upload to be at the top of the search
       sort: {
         direction: request.sort_order ?? 'asc',
         field: request.sort_key ?? 'original_file_name',
@@ -47,6 +48,7 @@ export const assetsApi = api.enhanceEndpoints({ addTagTypes: ['Assets'] }).injec
           : onErrorTags;
       },
     }),
+    // todo does this need to be deprecated ???
     addResource: builder.mutation<CreateAssetResponse, { newAsset: File; user: string }>({
       query: (prop) => {
         const requestBody = new FormData();
@@ -59,6 +61,20 @@ export const assetsApi = api.enhanceEndpoints({ addTagTypes: ['Assets'] }).injec
           headers: { 'x-api-version': '1' },
         };
       },
+    }),
+    createMedia: builder.mutation({
+      query: (mediaData) => ({
+        url: getAddAssetUrl(),
+        method: 'POST',
+        body: mediaData,
+      }),
+    }),
+    getSecureUploadUrl: builder.query({
+      query: (fileName) => ({
+        url: getSecureCloudUploadUrl(fileName),
+        method: 'GET',
+      }),
+      transformResponse: (response: { data: { signedUploadUrl: string }[] }) => response.data[0].signedUploadUrl,
     }),
     editResource: builder.mutation<UpdateAssetResponse, { id: string; original_file_name: string }>({
       query: (prop) => ({
@@ -89,10 +105,36 @@ export const assetsApi = api.enhanceEndpoints({ addTagTypes: ['Assets'] }).injec
   }),
 });
 
+// Make a call to Azure Blob Services to file upload
+export const uploadFileToBlob = async (file: File, signedUrl: string) => {
+  try {
+    const response = await fetch(signedUrl, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': file.type,
+        'x-ms-blob-type': 'BlockBlob',
+      },
+      body: file,
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to upload file: ${response.statusText}`);
+    }
+
+    console.log('File uploaded to Azure Blob storage.');
+    return true;
+  } catch (error) {
+    console.error('Error uploading to Blob storage:', error);
+    return false;
+  }
+};
+
 export const {
   useGetAssetsItemsQuery,
   useLazyGetAssetsItemsQuery,
+  useLazyGetSecureUploadUrlQuery,
   useAddResourceMutation,
   useEditResourceMutation,
   useDeleteResourceMutation,
+  useCreateMediaMutation,
 } = assetsApi;
